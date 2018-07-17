905fe443622e4018eb34373cf4e26c371bf1672c
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-17 15:50:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+configure do 
+  set :views, "views"
+  set :public_dir, "public"
+end 
 	get '/' do
-		
+		erb :index
 	end
+ get '/info' do
+   erb :info
+ end 
 end
