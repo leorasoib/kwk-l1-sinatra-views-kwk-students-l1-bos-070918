f83840c39a49ef3f7512c92e9b8888bd0d85b05c
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-17 15:46:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 configure do 
-  set: :views, "views"
-  set: :public_dir, "public"
+  set :views, "views"
+  set :public_dir, "public"
 end 
 	get '/' do
 		erb: :index
