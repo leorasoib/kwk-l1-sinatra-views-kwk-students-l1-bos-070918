966a2ebbf313ec7c4b9c7b08f8f2b00d8067fc5b
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-17 15:44:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,9 +4,11 @@ class App < Sinatra::Base
 configure do 
   set: :views, "views"
   set: :public_dir, "public"
-  
+end 
 	get '/' do
 		erb: :index
 	end
- 
+ get '/info' do
+ "Testing the info page"
+ end 
 end
